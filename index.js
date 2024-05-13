2db28bd75ae720c31e64d38cfac9fc6c222c3a81
Naimul Islum <naimulislum39@gmail.com>
2024-05-14 02:00:19 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -127,7 +128,16 @@ async function run() {
 
 
     //jwt
-    
+    app.post('/jwt' , async(req  , res) => {
+        const user = req.body;
+       const token =  jwt.sign(user , process.env.ACCESS_SECRET_TOKEN, {expiresIn : '1hr'} )
+        res.cookie('token' , token  , {
+            httpOnly : true,
+            sameSite : 'none',
+            secure: true
+        }).send({success: true})
+
+    })
     app.put("/myAddedPut/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
