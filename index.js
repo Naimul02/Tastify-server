5da6321e04ae7a0592dbe15b8d6c834d035e9ee7
Naimul Islum <naimulislum39@gmail.com>
2024-05-14 01:45:30 +0600

    implent jwt token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion , ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -25,97 +25,129 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const foodsCollection = client.db("assignment-11").collection("foods");
-    const allFoodsCollection = client.db("assignment-11").collection("allFoods");
-    const purchaseFoodsCollection = client.db("assignment-11").collection("purchase");
+    const allFoodsCollection = client
+      .db("assignment-11")
+      .collection("allFoods");
+    const purchaseFoodsCollection = client
+      .db("assignment-11")
+      .collection("purchase");
     const usersCollection = client.db("assignment-11").collection("users");
     const addFoodCollection = client.db("assignment-11").collection("addFood");
+    const galleryCollection = client.db("assignment-11").collection("gallery");
+
+    app.get("/foods", async (req, res) => {
+      const foods = foodsCollection.find();
+      const result = await foods.toArray();
+      res.send(result);
+    });
 
 
-    app.get('/foods' , async(req , res) =>{
-        const foods = foodsCollection.find();
-        const result = await foods.toArray()
-        res.send(result);
+    app.get("/foodsAll", async (req, res) => {
+      const foodsAll = allFoodsCollection.find();
+      const result = await foodsAll.toArray();
+      res.send(result);
+    });
+    app.get('/gallery' , async(req , res) => {
+        const gallery = await galleryCollection.find().toArray();
+        res.send(gallery)
     })
-
-    app.get('/foodsAll' , async(req , res) =>{
-        const foodsAll = allFoodsCollection.find();
-        const result = await foodsAll.toArray()
-        res.send(result);
+    app.post('/gallery' , async(req , res) => {
+      const cursor = req.body;
+        const gallery = await galleryCollection.insertOne(cursor)
+        res.send(gallery)
     })
-    app.get("/singleFood/:id" , async(req , res) => {
-        const id = req.params.id;
-        console.log(id);
+    app.get("/singleFood/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
 
-        const query = {_id : new ObjectId(id)};
-        const result = await allFoodsCollection.findOne(query);
-        res.send(result);
-    })
-    app.get("/myAddedFood" , async(req , res) => {
-        let query = {}
-        if(req.query?.email){
-            query={email : req.query.email}
-        }
-        // console.log(query);
-        const result= await addFoodCollection.find(query).toArray();
-        
-        // console.log(result)
-        res.send(result);
-    })
-    app.get('/update/:id' ,async(req , res) => {
-        const id = req.params.id;
-        console.log(id);
-        const filter = {_id : new ObjectId(id)};
-        const result = await addFoodCollection.findOne(filter);
-        res.send(result);
-    })
-    app.post('/purchase' , async(req , res) => {
-        const purchaseFood = req.body;
-        const result = await purchaseFoodsCollection.insertOne(purchaseFood);
-        res.send(result);
+      const query = { _id: new ObjectId(id) };
+      const result = await allFoodsCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/myAddedFood", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      // console.log(query);
+      const result = await addFoodCollection.find(query).toArray();
 
-    })
-    app.post('/users' , async(req , res) => {
-        const user = req.body;
-        
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
+      // console.log(result)
+      res.send(result);
+    });
+    app.delete("/myAddedFood/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addFoodCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.get("/purchaseOrderFood", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { buyerEmail: req.query.email };
+      }
+      console.log(query);
+      const result = await purchaseFoodsCollection.find(query).toArray();
 
-    })
-    app.post('/addFood' ,async(req , res) => {
-        const food = req.body;
-        const result = await addFoodCollection.insertOne(food);
-        res.send(result);
+      res.send(result);
+    });
+    app.delete("/purchaseOrderFood/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await purchaseFoodsCollection.deleteOne(query);
+      res.send(result);
+    });
+    
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const result = await addFoodCollection.findOne(filter);
+      res.send(result);
+    });
+    app.post("/purchase", async (req, res) => {
+      const purchaseFood = req.body;
+      const result = await purchaseFoodsCollection.insertOne(purchaseFood);
+      res.send(result);
+    });
 
-    })
-    app.put("/myAddedPut/:id" , async(req , res) => {
-          const id = req.params.id;
-          console.log(id);
-          const updatedFood = req.body;
-          const filter = {_id : new ObjectId(id)}
-          const updatedDoc = {
-              $set : {
-                  food_name : updatedFood.food_name,
-                  description : updatedFood.description,
-                  email : updatedFood.email,
-                  food_category : updatedFood.food_category,
-                  food_origin : updatedFood.food_origin,
-                  price : updatedFood.price,
-                  quantity : updatedFood.quantity,
-                  userName : updatedFood.userName
-              }
-          }
-          const result = await addFoodCollection.updateOne(filter , updatedDoc)
-          res.send(result);
+    app.post("/users", async (req, res) => {
+      const user = req.body;
 
-    })
-
-
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.post("/addFood", async (req, res) => {
+      const food = req.body;
+      const result = await addFoodCollection.insertOne(food);
+      res.send(result);
+    });
 
 
-
-
+    //jwt
+    
+    app.put("/myAddedPut/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const updatedFood = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          food_name: updatedFood.food_name,
+          description: updatedFood.description,
+          email: updatedFood.email,
+          food_category: updatedFood.food_category,
+          food_origin: updatedFood.food_origin,
+          price: updatedFood.price,
+          quantity: updatedFood.quantity,
+          userName: updatedFood.userName,
+        },
+      };
+      const result = await addFoodCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
